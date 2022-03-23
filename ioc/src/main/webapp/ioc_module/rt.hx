a8f1b91cda9ac6d3c3ed08f26b340d93412ac615
twoicefish15@gmail.com <twoicefish15@gmail.com>
2022-03-23 20:56:24 +0900

    LAST EDITED

--- ioc/src/main/webapp/ioc_module/rt.hx
+++
@@ -1,7 +0,0 @@
-{"igloo":{"execution":[
-[{"operator":"equal","token":"ipv4NetworkEvent/remoteIP","type":"text","value":"111.222.111.222"}],
-[{"operator":"contains","token":"dnsLookupEvent/hostname","type":"text","value":"https://www.sdifjsod.com"}],
-[{"operator":"contains","token":"urlMonitorEvent/hostname","type":"text","value":"https://www.sdifjsod.com"}]
-
-],"presence":[
-],"name":"rt","category":"Custom","platforms":["win","osx"]}}
